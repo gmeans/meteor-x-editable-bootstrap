e0223e172913bb40ef75d48423eadd89e3f15dc1
Grant Means <grant@ceroic.com>
2014-01-03 09:49:46 -0500

    Bootstrap 3 Commit

--- package.js
+++ package.js
@@ -7,22 +7,22 @@ Package.on_use(function (api){
 	api.use(['jquery'], 'client')
 
 	//x-editable
-	api.add_files('lib/bootstrap-editable/css/bootstrap-editable.css', 'client');
-	api.add_files('lib/bootstrap-editable/js/bootstrap-editable.js', 'client', {bare:true});
-	api.add_files('lib/bootstrap-editable/img/clear.png', 'client');
-	api.add_files('lib/bootstrap-editable/img/loading.gif', 'client');
+	api.add_files('lib/bootstrap3-editable/css/bootstrap-editable.css', 'client');
+	api.add_files('lib/bootstrap3-editable/js/bootstrap-editable.js', 'client', {bare:true});
+	api.add_files('lib/bootstrap3-editable/img/clear.png', 'client');
+	api.add_files('lib/bootstrap3-editable/img/loading.gif', 'client');
 	
 	//address
-	api.add_files('lib/address/address.css', 'client');
-	api.add_files('lib/address/address.js', 'client', {bare:true});
+	api.add_files('lib/inputs-ext/address/address.css', 'client');
+	api.add_files('lib/inputs-ext/address/address.js', 'client', {bare:true});
 
 	//wysihtml5
-	api.add_files('lib/wysihtml5/wysihtml5.js', 'client', {bare:true});  
-	api.add_files('lib/wysihtml5/bootstrap-wysihtml5-0.0.2/wysiwyg-color.css', 'client');
-	api.add_files('lib/wysihtml5/bootstrap-wysihtml5-0.0.2/wysihtml5-0.3.0.js', 'client', {bare:true});
-	api.add_files('lib/wysihtml5/bootstrap-wysihtml5-0.0.2/bootstrap-wysihtml5-0.0.2.css', 'client');
-	api.add_files('lib/wysihtml5/bootstrap-wysihtml5-0.0.2/bootstrap-wysihtml5-0.0.2.js', 'client', {bare:true});
+	api.add_files('lib/inputs-ext/wysihtml5/wysihtml5.js', 'client', {bare:true});  
+	api.add_files('lib/inputs-ext/wysihtml5/bootstrap-wysihtml5-0.0.2/wysiwyg-color.css', 'client');
+	api.add_files('lib/inputs-ext/wysihtml5/bootstrap-wysihtml5-0.0.2/wysihtml5-0.3.0.js', 'client', {bare:true});
+	api.add_files('lib/inputs-ext/wysihtml5/bootstrap-wysihtml5-0.0.2/bootstrap-wysihtml5-0.0.2.css', 'client');
+	api.add_files('lib/inputs-ext/wysihtml5/bootstrap-wysihtml5-0.0.2/bootstrap-wysihtml5-0.0.2.js', 'client', {bare:true});
 	
 	//override image paths
-	api.add_files('path-override.css', 'client');  
+	api.add_files('path-override3.css', 'client');  
 });
